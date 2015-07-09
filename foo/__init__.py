d8687aefa389f031abaff8dd17bd47c39d611c03
Danny Hermes <daniel.j.hermes@gmail.com>
2015-07-08 17:16:34 -0700

    Make the print-out deterministic.
    Does so by
    - Printing the module keys in alphabetical order
    - Mapping the encountered IDs to a re-numbered (at 0) list
      of IDs and remembering these by alphabetical order
      between runs

--- foo/__init__.py
+++ foo/__init__.py
@@ -1,12 +1,6 @@
 from __future__ import absolute_import
 import os
 import google
-# print('google:')
-# print(google)
-# print('id(google):')
-# print(id(google))
-# print('google.__path__:')
-# print(google.__path__)
 CURR_DIR = os.path.abspath(os.path.dirname(__file__))
 LOCAL_GOOGLE = os.path.join(CURR_DIR, 'google')
 google.__path__.append(LOCAL_GOOGLE)
--- import_tester.py
+++ import_tester.py
@@ -1,21 +1,39 @@
 import sys
-print('---- Before imports')
-for key, value in sys.modules.items():
-    if 'google' in key:
-        message = '    keyname: %20s, module is: %d' % (key, id(value))
+
+
+IDS_FOUND = {}
+
+
+def report_imports():
+    found = {}
+    for key, value in sys.modules.items():
+        if 'google' in key:
+            found[key] = value
+
+    if IDS_FOUND:
+        id_renumber = max(IDS_FOUND.values()) + 1
+    else:
+        id_renumber = 0
+
+    for key in sorted(found.keys()):
+        curr_id = id(found[key])
+        if curr_id not in IDS_FOUND:
+            IDS_FOUND[curr_id] = id_renumber
+            id_renumber += 1
+
+        id_val = IDS_FOUND[curr_id]
+        message = '    keyname: %20s, module is: %d' % (key, id_val)
         print(message)
+
+
+print('---- Before imports')
+report_imports()
 print('---------------------------------')
 from foo.google import baz
 print('---- After foo.google.baz import')
-for key, value in sys.modules.items():
-    if 'google' in key:
-        message = '    keyname: %20s, module is: %d' % (key, id(value))
-        print(message)
+report_imports()
 print('---------------------------------')
 from foo.google import bing
 print('---- After foo.google.bing import')
-for key, value in sys.modules.items():
-    if 'google' in key:
-        message = '    keyname: %20s, module is: %d' % (key, id(value))
-        print(message)
+report_imports()
 print('---------------------------------')
